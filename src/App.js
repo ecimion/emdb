4c444289cf7544174cc6de475955db7a3530138e
Christopher Calderon <emicion@gmail.com>
2019-07-08 13:22:50 -0700

    Add id links.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Movie from './Movie';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import MoviesList from './MoviesList';
 
-class App extends React.Component {
-
-  state = {
-    movies: []
-  }
-
-  async componentDidMount() {
-    try {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=c036ce0df8fcea4aa1b474761e9eae1b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-      const movies = await res.json();
-      this.setState({
-        movies: movies.results
-      })
-    } catch(e) {
-      console.log(e);
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
           <h1>[emdb]</h1>
-        </header>
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-      </div>
-    );
-
-  }
-}
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/test" component={Test} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
+
+const Test = () => (
+  <h1>TEST</h1>
+);
