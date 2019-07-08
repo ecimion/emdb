0e35e9a58c406ad772be9b8dd05b5bdc7d3bc9c5
Christopher Calderon <emicion@gmail.com>
2019-07-07 17:31:05 -0700

    Add API from The Movie Datebase website. Currently outputs movie titles from API.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=c036ce0df8fcea4aa1b474761e9eae1b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      })
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </div>
+    );
+
+  }
 }
 
 export default App;
